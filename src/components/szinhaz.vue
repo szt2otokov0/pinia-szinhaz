dcb84a3502c73056e790df4319333375aa222fad
Tamás Ótott-Kovács <otottkovi@hotmail.com>
2024-01-15 14:41:27 +0100

    some styling

--- src/components/szinhaz.vue
+++ src/components/szinhaz.vue
@@ -7,18 +7,53 @@ const currentSzinhaz = ref(szinhazStore.szinhaz['Vígszínház'])
 </script>
 
 <template>
-<h1>Hello!</h1>
-<div id="gombcontainer" v-for="szinhaz of szinhazStore.szinhaz" :key="szinhaz.nev">
-    <button @click="() => currentSzinhaz = szinhazStore.szinhaz[szinhaz.nev]">{{ szinhaz.nev }}</button>
-</div>
-<div id="info">
-    <h3>{{ currentSzinhaz.nev }}</h3>
-    <h4>Előadások:</h4>
-    <ul v-for="eloadas of currentSzinhaz.eloadasok">
-        <li>
-            <div>{{ eloadas.cim }}</div>
-            <a v-bind:href="eloadas.url">Előadás linkje</a>
-        </li>
-    </ul>
+<div id="szinhazcontainer">
+    <h1>Válasszon színházat!</h1>
+    <div id="gombcontainer">
+        <button @click="() => currentSzinhaz = szinhazStore.szinhaz[szinhaz.nev]" v-for="szinhaz of szinhazStore.szinhaz" :key="szinhaz.nev">
+            {{ szinhaz.nev }}
+        </button>
+    </div>
+    <div id="info">
+        <h3>{{ currentSzinhaz.nev }}</h3>
+        <h4>Előadások:</h4>
+        <ul v-for="eloadas of currentSzinhaz.eloadasok">
+            <li>
+                <div>{{ eloadas.cim }}</div>
+                <a v-bind:href="eloadas.url">Előadás linkje</a>
+            </li>
+        </ul>
+    </div>
 </div>
 </template>
+
+<style scoped>
+#szinhazcontainer {
+    display: flex;
+    flex-direction: column;
+    align-items: center;
+    justify-content: space-around;
+    font-family: 'Gill Sans', 'Gill Sans MT', Calibri, 'Trebuchet MS', sans-serif;
+}
+
+#gombcontainer {
+    display:flex;
+    flex-direction: row;
+}
+
+button {
+    margin: 1em;
+    padding: 0.5em;
+    border: 1px solid black;
+    border-radius: 5px;
+    font-weight: 500;
+    
+}
+
+#info {
+    border: 1px solid black;
+    border-radius: 5px;
+    padding: 0 1em;
+    margin-top: 1em;
+}
+</style>
